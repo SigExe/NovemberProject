96f87001cb932bda67c821556cfb3d3b9b66bb00
SigExe <SIG96@protonmail.com>
2020-11-01 09:53:57 -0600

    joker

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -2,6 +2,9 @@ public class Batman {
 
     public static void main(String[] args) {
         System.out.println("Bruh of Bats");
+
+        System.out.println("I'll catch you, Joker, lmao");
+
     }
 
 }
