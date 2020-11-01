cd1c672f18c3453bb6720973c4410bd3218d44e1
SigExe <SIG96@protonmail.com>
2020-11-01 10:20:47 -0600

    super good

--- src/main/java/Superman.java
+++ src/main/java/Superman.java
@@ -2,6 +2,9 @@ public class Superman {
 
     public static void main(String[] args) {
         System.out.println("Bruh of Steel");
+
+        System.out.println("Bruh of Steel needs love too");
+
     }
 
 }
